fbe56a3eaa53fdadb9783a09192aea248bca6cee
James Kovacs <jkovacs@post.harvard.edu>
2014-06-05 14:25:10 -0600

    Implemented simple addition questions

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,17 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
-
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    $scope.operand1 = getRandomInt(0, 100);
+    $scope.operand2 = getRandomInt(0, 100);
+    $scope.answerStatus = "unanswered";
 
+    $scope.checkResult = function() {
+      var correctAnswer = ($scope.operand1 + $scope.operand2 == $scope.result);
+      $scope.answerStatus = correctAnswer ? "correct" : "incorrect";
+    };
   }]);
