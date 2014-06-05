8ed72233ef2a2d865ebab7863f69abc90699508a
Craig Anderson <craig@simplyevolvedsoftware.com>
2014-06-05 14:05:36 -0600

    Added a simple addition question to be answered and it will return a result.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,16 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
+        $scope.variable1 = 2;
+        $scope.variable2 = 2;
+        $scope.answer = $scope.variable1 + $scope.variable2;
+        $scope.validateAnswer =function(){
+            if(parseInt($scope.childAnswer) === $scope.answer){
+                $scope.answerResult = "You're Correct!";
+            }else{
+                $scope.answerResult = "Sorry, try again.";
+            }
+        };
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
