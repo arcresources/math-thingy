83a59285f3b5df6800ec53a575416f90e3f75918
Ben Guenter <bguenter@arcresources.com>
2014-06-12 15:06:04 -0600

    Added a generate button, no error messages yet :(

--- app/js/controllers/ben-controller.js
+++ app/js/controllers/ben-controller.js
@@ -1,3 +1,12 @@
 angular.module('myApp.controllers').controller('BenCtrl', ['$scope', '$http', function($scope, $http){
-	$scope.MyTest = 'Testing This!';
+	$scope.GenerateRandomMathProblem = function(){
+		var operandList = ['+','-','/','*'];
+		var operand = operandList[Math.floor(Math.random() * operandList.length)]
+		var number =  $scope.GenerateNumberBetweenMinAndMax();
+		$scope.Question = number+" "+operand;
+	};
+
+	$scope.GenerateNumberBetweenMinAndMax = function(){
+		return Math.floor(Math.random() * (this.Parameters.MaxNumber - this.Parameters.MinNumber + 1) + this.Parameters.MinNumber)
+	}
 }]);
