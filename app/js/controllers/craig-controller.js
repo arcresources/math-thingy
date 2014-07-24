e587c13d6cdb4bdc0e2d532b5e7572bcf531ed0b
Craig Anderson <craig@simplyevolvedsoftware.com>
2014-07-24 14:04:38 -0600

    Added a way to get the answer and clear the prior question's information.

--- app/js/controllers/craig-controller.js
+++ app/js/controllers/craig-controller.js
@@ -1,5 +1,6 @@
 angular.module('myApp.controllers')
     .controller('CraigCtrl', ['$scope', function($scope) {
+        $scope.shouldShowCorrectAnswer = false;
         newQuestion($scope);
         $scope.newQuestion = function(){
             newQuestion();
@@ -8,13 +9,20 @@ angular.module('myApp.controllers')
         $scope.validateAnswer =function(){
             if(parseInt($scope.childAnswer) === $scope.answer){
                 $scope.answerResult = "You're Correct!";
+                $scope.displayShowCorrectAnswerButton = false;
             } else {
                 $scope.answerResult = "Sorry, try again.";
+                $scope.displayShowCorrectAnswerButton = true;
             }
         };
         function newQuestion(){
             $scope.variable1 = Math.floor(Math.random() * 100);
             $scope.variable2 = Math.floor(Math.random() * 100);
             $scope.answer = $scope.variable1 + $scope.variable2;
+            $scope.answerResult = "";
+            $scope.childAnswer = "";
+        };
+        $scope.displayCorrectAnswer = function(){
+            $scope.shouldShowCorrectAnswer = true;
         };
     }]);
